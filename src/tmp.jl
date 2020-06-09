dda7920c948697eb29cfa10b0028b544e3662de4
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-06-09 10:23:07 -0700

    modify tmp function

--- src/tmp.jl
+++ src/tmp.jl
@@ -1,4 +1,12 @@
 
-function tmp()
-    CSV.read(datadep"medium_main.csv")
+function tmp(code)
+    if code == 1
+        CSV.read(datadep"medium_main.csv")
+    elseif code == 2
+        CSV.read(datadep"medium_controls_main.csv")
+    elseif code == 3
+        CSV.read(datadep"full_main.csv")
+    elseif code == 4
+        CSV.read(datadep"full_controls_main.csv")
+    end
 end
