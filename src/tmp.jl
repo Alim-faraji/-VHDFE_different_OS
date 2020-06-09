5d51676cc6da765fb44ae4a30fc1ea3a8d0268a9
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-06-09 10:27:06 -0700

    fix another typo

--- src/tmp.jl
+++ src/tmp.jl
@@ -1,12 +1,12 @@
 
 function tmp(code)
     if code == 1
-        CSV.read(datadep"medium_main.csv")
+        CSV.read(datadep"VarianceComponentsHDFE/medium_main.csv")
     elseif code == 2
-        CSV.read(datadep"medium_controls_main.csv")
+        CSV.read(datadep"VarianceComponentsHDFE/medium_controls_main.csv")
     elseif code == 3
-        CSV.read(datadep"full_main.csv")
+        CSV.read(datadep"VarianceComponentsHDFE/full_main.csv")
     elseif code == 4
-        CSV.read(datadep"full_controls_main.csv")
+        CSV.read(datadep"VarianceComponentsHDFE/full_controls_main.csv")
     end
 end
