022c70e8d5126ba3a5661604fc79e4c565d9802a
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-07-17 15:06:09 -0700

    wrap the LDLinv object as a preallocated linear operator

--- src/VarianceComponentsHDFE.jl
+++ src/VarianceComponentsHDFE.jl
@@ -4,14 +4,17 @@ using DataDeps, CSV
 using DataFrames, DataFramesMeta, Parameters, AlgebraicMultigrid
 using LinearAlgebra, SparseArrays, Random, Statistics
 using SparseArrays, IterativeSolvers, LightGraphs, VectorizedRoutines, CSVFiles, DataFramesMeta, Laplacians
+using LinearOperators, FastClosures
 
 include("init.jl")
 include("leave_out_correction.jl")
 include("parameters_settings.jl")
+include("solver.jl")
 
 export drop_single_obs
 export lss
 export initialize_auxiliary_variables
 export Settings, CMGPreconditionedLLS, AMGPreconditionedLLS, DirectLLS, JLAAlgorithm
+export approxCholOperator
 
 end
---
+++ src/solvers.jl
@@ -0,0 +1,12 @@
+
+
+# Wrap the LDLinv object as a preallocated linear operator
+function approxCholOperator(ldli::Laplacians.LDLinv{Tind,Tval},buff::Vector{Tval}) where {Tind,Tval}
+    prod = @closure rhs -> LDLsolver!(buff,ldli,rhs)
+    return PreallocatedLinearOperator{Tval}(length(ldli.d), length(ldli.d), true, true, prod, nothing, nothing)
+end
+
+# Call the Kyrlov cg using the LDLinv preallocated linear operator
+function solveApproxChol()
+ return nothing
+end
