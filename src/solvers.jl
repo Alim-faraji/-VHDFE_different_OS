cb807ff7cdbb51f65f0f37913a8d5249d2d8d14a
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-07-17 18:32:54 -0700

    finish the approxchol refactoring with Krylov.jl

--- src/solvers.jl
+++ src/solvers.jl
@@ -6,7 +6,24 @@ function approxcholOperator(ldli::Laplacians.LDLinv{Tind,Tval},buff::Vector{Tval
     return PreallocatedLinearOperator{Tval}(length(ldli.d), length(ldli.d), true, true, prod, nothing, nothing)
 end
 
-# Call the Kyrlov cg using the LDLinv preallocated linear operator
-function solveApproxChol()
- return nothing
+# Call the Kyrlov cg using the LDLinv preallocated linear operator on an sddm system
+function solveApproxChol(sddm::AbstractArray, P; tol::Real=1e-6, maxits=300, maxtime=Inf, verbose=false, pcgIts=Int[], params...)
+    a,d = adj(sddm)
+    a1 = Laplacians.extendMatrix(a,d)
+    la = lap(a1)
+
+    tol_=tol
+    maxits_=maxits
+    maxtime_=maxtime
+    verbose_=verbose
+    pcgIts_=pcgIts
+
+    f = function(b;tol=tol_, maxits=maxits_, maxtime=maxtime_, verbose=verbose_, pcgIts=pcgIts_)
+        xaug = Krylov.cg(la,[b; -sum(b)] .- Laplacians.mean([b; -sum(b)]), M=P, rtol = tol_, itmax=maxits_ ,verbose=verbose_)[1]
+        xaug = xaug .- xaug[end]
+        return xaug[1:a.n]
+    end
+
+    return f
+
 end
