9334145256b08f65bd7f2be3182d04ea65ee4205
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-07-20 11:06:27 -0700

    touch up the solver function

--- src/solvers.jl
+++ src/solvers.jl
@@ -6,20 +6,20 @@ function approxcholOperator(ldli::Laplacians.LDLinv{Tind,Tval},buff::Vector{Tval
     return PreallocatedLinearOperator{Tval}(length(ldli.d), length(ldli.d), true, true, prod, nothing, nothing)
 end
 
-# Call the Kyrlov cg using the LDLinv preallocated linear operator on an sddm system
-function solveApproxChol(sddm::AbstractArray, P; tol::Real=1e-6, maxits=300, maxtime=Inf, verbose=false, pcgIts=Int[], params...)
+# Returns a function that calls Krylov.cg using the LDLinv preconditioner wrapped as a preallocated linear operator
+#  on a SDDM system (grounded case)
+function solveApproxChol(sddm::AbstractArray, P; tol::Real=1e-6, maxits=300, verbose=false)
     a,d = adj(sddm)
     a1 = Laplacians.extendMatrix(a,d)
     la = lap(a1)
 
     tol_=tol
     maxits_=maxits
-    maxtime_=maxtime
     verbose_=verbose
-    pcgIts_=pcgIts
 
-    f = function(b;tol=tol_, maxits=maxits_, maxtime=maxtime_, verbose=verbose_, pcgIts=pcgIts_)
-        xaug = Krylov.cg(la,[b; -sum(b)] .- Laplacians.mean([b; -sum(b)]), M=P, rtol = tol_, itmax=maxits_ ,verbose=verbose_)[1]
+
+    f = function(b;tol=tol_, maxits=maxits_, verbose=verbose_)
+        xaug = Krylov.cg(la,[b; -sum(b)] .- Laplacians.mean([b; -sum(b)]), M=P, rtol = tol, itmax=maxits, verbose=verbose)[1]
         xaug = xaug .- xaug[end]
         return xaug[1:a.n]
     end
