15cc2aa1c2c8575923cf187ce8f4a86e98531afc
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-07-22 14:45:31 -0700

    update VarianceComponentsHDFE.jl

--- src/VarianceComponentsHDFE.jl
+++ src/VarianceComponentsHDFE.jl
@@ -5,12 +5,15 @@ using DataFrames, DataFramesMeta, Parameters, AlgebraicMultigrid
 using LinearAlgebra, SparseArrays, Random, Statistics
 using SparseArrays, IterativeSolvers, LightGraphs, VectorizedRoutines, CSVFiles, DataFramesMeta
 using LinearOperators, FastClosures, Krylov
-using .Laplacians
+
 
 include("init.jl")
 include("leave_out_correction.jl")
 include("parameters_settings.jl")
 include("solvers.jl")
+include("laplacians/Laplacians.jl")
+
+using .Laplacians
 
 export drop_single_obs
 export lss
