a6b32e46ef4dafca6e84921ab8a3486c75275db0
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-07-22 14:39:16 -0700

    fix afew more things

--- src/laplacians/Laplacians.jl
+++ src/laplacians/Laplacians.jl
@@ -10,6 +10,7 @@ module Laplacians
   include("approxCholTypes.jl")
   include("approxChol.jl")
   include("graphOps.jl")
+  include("graphUtils.jl")
 
   export adj, lap, extendMatrix, approxchol_lap_pc, LDLsolver!, LDLinv
 
