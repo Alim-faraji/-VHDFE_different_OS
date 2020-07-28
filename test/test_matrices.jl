fc314a6925752d88fe8cc5aa8cced90c340f3ea3
paulcorcuera <paulcorcuera.g@gmail.com>
2020-07-28 11:19:53 -0700

    Update test_matrices.jl

--- test/test_matrices.jl
+++ test/test_matrices.jl
@@ -8,6 +8,7 @@ using LinearAlgebra, SparseArrays,Statistics,DataFrames,DataFramesMeta
 #include(string(Pkg.dir("Laplacians") , "/src/matlabSolvers.jl"))
 using Test
 using Parameters
+using Random
 
 #Full Network
 const y_full = [0.146297; 0.29686 ;  0.54344; 0.432677 ; 0.464866 ; 0.730622 ; 0.619239; 0.753429; 0.0863208; 0.372084 ;  0.958089]
@@ -37,8 +38,6 @@ const match_id = [1; 2 ; 3;3 ; 4; 5; 6; 6]
 
     @test drop_single_obs(y_p,id_p,firmid_p,obs_id_p) == (obs_id = obs_id_lo1 , y = y_lo , id = id_lo, firmid = firmid_lo )
 
-    @test index_constr(obs_id_lo , id_lo, match_id) == [obs_id_lo  obs_id_lo  [1;2;3;3;4;5;6;6]   obs_id_lo]
-    @test index_constr(match_id , id_lo, match_id) == [[1;2;3;3;4;5;6;7;7;8]  [1;2;3;4;4;5;6;7;8;8]  [1;2;3;3;3;4;5;6;6;6]   [1;2;3;3;3;4;5;6;6;6]  ]
 end
 
 @testset "Movers & Matches" begin 
@@ -53,24 +52,34 @@ compute_sol = approxcholSolver(xxtest;verbose=true)
 settings_exact = Settings(leverage_algorithm = ExactAlgorithm(), person_effects=true, cov_effects=true)
 settings_JLA = Settings(leverage_algorithm = JLAAlgorithm(), person_effects=true, cov_effects=true)
 
+@testset "Clustering" begin
+    @test check_clustering(id_lo).nnz_2 == 12
 
-@test check_clustering(id_lo).nnz_2 == 12
+    @test check_clustering(match_id).nnz_2 ==10
 
-@test check_clustering(match_id).nnz_2 ==10
+    @test index_constr(obs_id_lo,id_lo, obs_id_lo) == [obs_id_lo  obs_id_lo  obs_id_lo   obs_id_lo]
 
-@test index_constr(obs_id_lo,id_lo, obs_id_lo) == [obs_id_lo  obs_id_lo  obs_id_lo   obs_id_lo]
-
-@test index_constr(obs_id_lo,id_lo, match_id) == [obs_id_lo  obs_id_lo  [1;2;3;3;4;5;6;6]   obs_id_lo]
-
-
-
-#@testset "Solvers" begin 
-#    compute_sol( [Xtest[1,:]...] ;verbose=true) ≈ [ 0.25, -0.5, -0.25, 0.0, -0.5] 
-#end
-
-@testset "LambdaMatrices" begin 
-    @test    do_Pii(Xtest,obs_id_lo) == sparse( [1,2,3,4,5,6,7,8], [1,2,3,4,5,6,7,8], [0.75,0.75,0.5,0.5,0.75,0.75,0.5,0.5] )
+    @test index_constr(obs_id_lo,id_lo, match_id) == [obs_id_lo  obs_id_lo  [1;2;3;3;4;5;6;6]   obs_id_lo]
 
 end
 
-eff_res(settings_exact.leverage_algorithm, X,id,firmid,match, K, settings_exact)
+
+Random.seed!(1234)
+rademacher1 = rand(1,8)  .> 0.5
+rademacher1 = rademacher1 - (rademacher1 .== 0)
+
+@testset "Solvers" begin 
+    @test compute_sol( [Xtest[1,:]...] ;verbose=true) ≈ [ 0.25, -0.5, -0.25, 0.0, -0.5] 
+    @test compute_sol([rademacher1*Xtest...];verbose=true) ≈ [ 1.0, 0.0, 1.0, -1.0, 0.0] 
+end
+
+@testset "LambdaMatrices" begin 
+    @test    do_Pii(Xtest,obs_id_lo) == sparse( [1,2,3,4,5,6,7,8], [1,2,3,4,5,6,7,8], [0.75,0.75,0.5,0.5,0.75,0.75,0.5,0.5] )
+    #@test    eff_res(settings_exact.leverage_algorithm, X,id,firmid,match, K, settings_exact).Lambda_P == sparse( [1,2,3,4,5,6,7,8], [1,2,3,4,5,6,7,8], [0.75,0.75,0.5,0.5,0.75,0.75,0.5,0.5] )
+    #@test    eff_res(settings_JLA.leverage_algorithm, X,id,firmid,match, K, settings_JLA).Lambda_P ≈ sparse( [1,2,3,4,5,6,7,8], [1,2,3,4,5,6,7,8], [0.75,0.75,0.5,0.5,0.75,0.75,0.5,0.5] )
+end
+
+@testset "LeaveOut" begin 
+
+end
+
