649388d1408c9638e9070620f33216244b55ffb0
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-08-11 10:20:44 -0700

    add residualized controls

--- src/VarianceComponentsHDFE.jl
+++ src/VarianceComponentsHDFE.jl
@@ -6,6 +6,7 @@ using LinearAlgebra, SparseArrays, Random, Statistics
 using SparseArrays, LightGraphs, VectorizedRoutines, CSVFiles, DataFramesMeta
 using Distributions, Arpack
 using LinearOperators, FastClosures, Krylov
+using IterativeSolvers, AlgebraicMultigrid
 
 include("init.jl")
 include("leave_out_correction.jl")
@@ -17,7 +18,7 @@ include("solvers.jl")
 
 export find_connected_set,prunning_connected_set,drop_single_obs, index_constr
 export compute_movers, check_clustering, eff_res
-export do_Pii, lincom_KSS, compute_matchid, leave_out_estimation
+export do_Pii, lincom_KSS, compute_matchid, leave_out_estimation, compute_whole
 export Settings, JLAAlgorithm, ExactAlgorithm, AbstractLeverageAlgorithm
 
 # Exporting these functions for ease of benchmarking/testing
