e1c73ea530ddb35167e93def63aef239a5c0c0b6
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-08-19 16:50:44 -0700

    make toy julia_make functions for app

--- src/VarianceComponentsHDFE.jl
+++ src/VarianceComponentsHDFE.jl
@@ -24,4 +24,27 @@ export Settings, JLAAlgorithm, ExactAlgorithm, AbstractLeverageAlgorithm
 # Exporting these functions for ease of benchmarking/testing
 export computeLDLinv, approxcholOperator, approxcholSolver
 
+function julia_main()::Cint
+    try
+        real_main()
+    catch
+        Base.invokelatest(Base.display_error, Base.catch_stack())
+        return 1
+    end
+    return 0
+end
+
+function real_main()::Cint
+    data = CSV.read(ARGS[1];header=false)
+    id = data[:,1]
+    firmid = data[:,2]
+    y = data[:,4]
+    controls = nothing
+    println("successfully opened the CSV")
+    settings = Settings(leverage_algorithm = JLAAlgorithm(num_simulations=parse(Int,ARGS[2])), person_effects=true, cov_effects=true)
+    θFE, θPE, θCOV = compute_whole(y,id,firmid,controls,settings;verbose=false)
+    println((θFE, θPE, θCOV))
+    return 0
+end
+
 end
