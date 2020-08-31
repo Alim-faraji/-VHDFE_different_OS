c3b5c862fbbe0d7b27c99d7043b0e9870178dcf1
Adam Jozefiak <adam.jozefiak@alumni.ubc.ca>
2020-08-31 11:37:46 -0700

    remove audit=false and add precompilation statements file

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
 import Pkg
 Pkg.add("PackageCompiler")
 using PackageCompiler
-create_app(".", "deps/VarianceComponentsHDFELinux", force=true, audit=false)
+create_app(".", "deps/VarianceComponentsHDFELinux", force=true, precompile_statements_file="deps/precompilation_statements.jl")
