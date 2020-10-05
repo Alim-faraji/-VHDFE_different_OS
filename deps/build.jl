71d96c4ffdde6f1ee340a4627f2ebb2b74af5e93
Alim-Faraji <71057115+Alim-faraji@users.noreply.github.com>
2020-10-05 13:01:36 -0700

    remove PackageCompiler from build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,2 @@
-import Pkg
-Pkg.add("PackageCompiler")
 using PackageCompiler
 create_app(".", "deps/VarianceComponentsHDFELinux", force=true, precompile_statements_file="deps/precompilation_statements.jl")
